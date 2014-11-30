6cba8561d881ae8d7a00ea68292c2b008813227a
erivanels@gmail.com <erivanels@gmail.com@acc91d75-689d-d992-e658-bc2c106b11b9>
2014-11-30 15:57:52 +0000

    model tipo enum

--- trunk/MySurvey/teste/Models/QuestaoModel.cs
+++ trunk/MySurvey/teste/Models/QuestaoModel.cs
@@ -7,31 +7,30 @@ namespace Modelos
 {
     public class QuestaoModel
     {
-        enum Tipo
-        {
-            Objetiva,
-            Subjetiva
-        };
+        //enum Tipo
+        //{
+        //    Objetiva,
+        //    Subjetiva
+        //};
 
-        enum Linguagem
-        {
-            CSharp,
-            Java,
-            PHP
-        };
+        //enum Linguagem
+        //{
+        //    CSharp,Java,PHP
+        //};
 
         public int id_Questao { get; set; }
         public int idTB_ITENS_DA_QUESTAO { get; set; }
         public int id_Survey { get; set; }
-        //public Enum Tipo { get; set; }
+        public string Tipo { get; set; }
         public String Pergunta { get; set; }
         public byte[] Img { get; set; }
         public Boolean Randomica { get; set; }
         public Boolean Obrigatoria { get; set; }
         public Boolean Codigo { get; set; }
-        //public Enum Linguagem { get; set; }
-
-
+        public string Linguagem { get; set; }
+     
     }
+    public enum Tipo {  Objetiva,Subjetiva };
+    public enum Linguagem { CSharp, Java, PHP };
 
 }
