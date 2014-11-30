d24c7d7a7b3622a7b1fdd1d2bb2ca5246b49b260
antoniodouglas.si <antoniodouglas.si@gmail.com>
2014-11-30 03:43:07 +0000

    Persistence feito e o service em andamento. Além de reorganização do projeto

---
+++ MySurvey/teste/Models/ResponsavelModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Modelos
+{
+    public class ResponsavelModel
+    {
+        public int id_Responsavel { get; set; }
+        public String nome { get; set; }
+        public String email { get; set; }
+        public String sobrenome { get; set; }
+    }
+}
